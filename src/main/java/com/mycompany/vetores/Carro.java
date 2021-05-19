e641af610d7e9698b7e8052a40aee3a89ec39a13
EveraldoTorres1984 <>
2021-05-19 15:13:57 -0300

    Estudo vetores - modelo

---
+++ src/main/java/com/mycompany/vetores/Carro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.vetores;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author torre
+ */
+public class Carro {
+
+    public void CadastroCarro() {
+        String marca[] = new String[3];
+        String modelo[] = new String[3];
+        String result = "";
+        for (int i = 0; i < marca.length; i++) {
+            marca[i] = JOptionPane.showInputDialog("Marca: ");
+            modelo[i] = JOptionPane.showInputDialog("Modelo: ");
+            result += marca[i] + " - " + modelo[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, result);
+
+        int indice = Integer.parseInt(JOptionPane.showInputDialog("Código da marca(1 a 3): "));
+        JOptionPane.showMessageDialog(null, marca[indice] + " - " + modelo[indice]);
+    }
+}
